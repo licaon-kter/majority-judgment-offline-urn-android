8e368d872378c400301a08a7f1365130847c194f
William Guillermin <guillermin.w@gmail.com>
2025-03-05 14:36:51 +0100

    chore: Added Preview to ThemedHorizontalDivider

--- app/src/main/java/com/illiouchine/jm/ui/composable/ThemedHorizontalDivider.kt
+++ app/src/main/java/com/illiouchine/jm/ui/composable/ThemedHorizontalDivider.kt
@@ -1,10 +1,17 @@
 package com.illiouchine.jm.ui.composable
 
+import android.content.res.Configuration
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.illiouchine.jm.ui.theme.JmTheme
 import com.illiouchine.jm.ui.theme.SeparatorDark
 import com.illiouchine.jm.ui.theme.SeparatorLight
 
@@ -16,4 +23,29 @@ fun ThemedHorizontalDivider(
         color = if (isSystemInDarkTheme()) SeparatorDark else SeparatorLight,
         modifier = modifier.fillMaxWidth(),
     )
+}
+
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL,
+)
+@Composable
+private fun DarkTehemedHorizontalDivider() {
+    JmTheme {
+        Box(modifier = Modifier.size(200.dp), contentAlignment = Alignment.Center) {
+            ThemedHorizontalDivider()
+        }
+    }
+}
+
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_NORMAL,
+    showBackground = true
+)
+@Composable
+private fun LightTehemedHorizontalDivider() {
+    JmTheme {
+        Box(modifier = Modifier.size(200.dp), contentAlignment = Alignment.Center) {
+            ThemedHorizontalDivider()
+        }
+    }
 }
